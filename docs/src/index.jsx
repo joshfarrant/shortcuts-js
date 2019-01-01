1f85848fafa18c8a96ec7694a64f089a4d722328
xAlien95 <xalien95@live.it>
2019-01-01 08:36:40 +0100

    Docs: removed typography.js

--- docs/src/index.jsx
+++ docs/src/index.jsx
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 
-import Typography from 'typography';
-
 import './styles/base.scss';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
@@ -18,33 +16,3 @@ ReactDOM.render((
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
 serviceWorker.unregister();
-
-
-const typography = new Typography({
-  baseFontSize: '18px',
-  baseLineHeight: 1.7,
-  headerFontFamily: [
-    '-apple-system',
-    'BlinkMacSystemFont',
-    'Segoe UI',
-    'Roboto',
-    'Oxygen-Sans',
-    'Ubuntu',
-    'Cantarell',
-    'Helvetica Neue',
-    'sans-serif',
-  ],
-  bodyFontFamily: [
-    '-apple-system',
-    'BlinkMacSystemFont',
-    'Segoe UI',
-    'Roboto',
-    'Oxygen-Sans',
-    'Ubuntu',
-    'Cantarell',
-    'Helvetica Neue',
-    'sans-serif',
-  ],
-});
-
-typography.injectStyles();

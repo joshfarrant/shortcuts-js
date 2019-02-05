3f2f11c81a0dd8d1a4b6b4bc0350c5e47b6f881e
Josh Farrant <josh@farrant.me>
2019-02-05 06:47:58 +0000

    Added tests for openApp action

--- src/actions/openApp.ts
+++ src/actions/openApp.ts
@@ -1,30 +1,30 @@
 import WFWorkflowAction from '../interfaces/WF/WFWorkflowAction';
 
 /**
- * Open the specified app using a bundle id.
+ * @action Open App
+ * @section Content Types > Apps >
+ * @icon Apps
+ *
+ * Opens the specified app.
  *
  * ```js
  * openApp({
- *   appID: "com.christianselig.Apollo"
+ *   appId: 'com.christianselig.Apollo'
  * });
  * ```
  */
 
 const openApp = (
-  options: {
-    appID?: string;
+  {
+    appId = '',
+  }: {
+    appId?: string;
   },
-): WFWorkflowAction => {
-  const {
-    appID = 'com.christianselig.Apollo',
-  } = options;
-
-  return {
-    WFWorkflowActionIdentifier: 'is.workflow.actions.openapp',
-    WFWorkflowActionParameters: {
-      WFAppIdentifier: appID,
-    },
-  };
-};
+): WFWorkflowAction => ({
+  WFWorkflowActionIdentifier: 'is.workflow.actions.openapp',
+  WFWorkflowActionParameters: {
+    WFAppIdentifier: appId,
+  },
+});
 
 export default openApp;

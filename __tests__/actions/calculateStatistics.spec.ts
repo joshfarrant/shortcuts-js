73505ea32dcb4b8e18f5a31ab20cb967328448b4
Shekhar Bhandakkar <cs14btech11006@iith.ac.in>
2019-03-04 16:13:11 +0530

    Added tests for calculateStatistics action

--- __tests__/actions/calculateStatistics.spec.ts
+++ __tests__/actions/calculateStatistics.spec.ts
@@ -1,9 +1,20 @@
 import { calculateStatistics } from '../../src/actions';
 
+import WFStatisticsOperation from '../../src/interfaces/WF/WFStatisticsOperation';
+import WFWorkflowActionIdentifier from '../../src/interfaces/WF/WFWorkflowActionIdentifier';
+
 describe('calculateStatistics function', () => {
 
   it('is a function', () => {
     expect(typeof calculateStatistics).toBe('function');
+  });
+
+  it('has an identifier property, which is a string', () => {
+    expect(typeof calculateStatistics.identifier).toBe('string');
+  });
+
+  it('has an invert property, which is a function', () => {
+    expect(typeof calculateStatistics.invert).toBe('function');
   });
 
   it('builds a calculateStatistics action', () => {
@@ -30,6 +41,23 @@ describe('calculateStatistics function', () => {
     });
 
     expect(actual).toEqual(expected);
+  });
+
+  it('inverts a calculateStatistics action', () => {
+    if (calculateStatistics.invert) {
+      const action = {
+        WFWorkflowActionIdentifier: 'is.workflow.actions.statistics' as WFWorkflowActionIdentifier,
+        WFWorkflowActionParameters: {
+          WFStatisticsOperation: 'Average' as WFStatisticsOperation,
+        },
+      };
+      const actual = calculateStatistics.invert(action);
+      const expected = {
+        operation: 'Average',
+      };
+
+      expect(actual).toEqual(expected);
+    }
   });
 
 });

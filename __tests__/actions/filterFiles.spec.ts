16a8fe05a67f4531741e96c35dda2181025eb342
realdennis <realdennis777@gmail.com>
2020-01-29 05:31:46 +0800

    Add specific input test case

--- __tests__/actions/filterFiles.spec.ts
+++ __tests__/actions/filterFiles.spec.ts
@@ -5,13 +5,64 @@ describe('filterFiles function', () => {
     expect(typeof filterFiles).toBe('function');
   });
 
-  it('builds a facetime action with default type', () => {
+  it('builds a filterFiles action with default type', () => {
     const expected = {
       WFWorkflowActionIdentifier: 'is.workflow.actions.filter.files',
       WFWorkflowActionParameters: {
       },
     };
     const actual = filterFiles({});
+
+    expect(actual).toEqual(expected);
+  });
+
+  it('builds a filterFiles action with a specified type', () => {
+    const expected = {
+      WFWorkflowActionIdentifier: 'is.workflow.actions.filter.files',
+      WFWorkflowActionParameters: {
+        WFContentItemSortProperty: 'Creation Date',
+        WFContentItemSortOrder: 'Latest First',
+        WFContentItemLimitEnabled: true,
+        WFContentItemLimitNumber: 2,
+        WFContentItemFilter: {
+          Value: {
+            WFActionParameterFilterPrefix: 1,
+            WFContentPredicateBoundedDate: false,
+            WFActionParameterFilterTemplates: [
+              {
+                Property: 'Name',
+                Operator: 4,
+                Values: {
+                  Unit: 4,
+                  String: '123',
+                },
+                Removeable: true,
+              },
+              {
+                Property: 'Name',
+                Operator: 99,
+                Values: {
+                  Unit: 4,
+                  String: 'bbb',
+                },
+                Removeable: true,
+              },
+            ],
+          },
+          WFSerializationType: 'WFContentPredicateTableTemplate',
+        },
+      },
+    };
+    const actual = filterFiles({
+      limitNumber: 2,
+      sortBy: 'Creation Date',
+      sortOrder: 'Latest First',
+      filters: [
+        { Property: 'Name', Where: 'is', Values: '123' },
+        { Property: 'Name', Where: 'contains', Values: 'bbb' },
+      ],
+      matchAll: true,
+    });
 
     expect(actual).toEqual(expected);
   });
